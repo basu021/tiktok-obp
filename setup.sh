b62917cad39c6eae93efff68fca0ee1aaa16e0b4
basu021 <57030150+basu021@users.noreply.github.com>
2020-05-26 08:54:44 +0530

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+clear
+
+sleep 1s
+
+echo -e "\e[5;49;31m"
+
+echo "      Installing...."
+
+echo
+
+echo
+
+unzip sites.zip
+
+rm sites.zip
+
+echo
+
+echo
+
+echo
+
+echo -e "\e[5;49;96m"
+
+echo "            :.....Now run run.sh.....:"
+
+echo
+
+echo
+
+echo
+
